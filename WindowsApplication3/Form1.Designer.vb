9fc30b30421d1b59e8db18156fd2cb0f3e5d74b6
johan alexis <johanalexis1201@outlook.com>
2024-06-05 22:04:56 -0500

    Agregar archivos de proyecto.

---
+++ WindowsApplication3/Form1.Designer.vb
@@ -0,0 +1,212 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.RadioButton3 = New System.Windows.Forms.RadioButton()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.RadioButton2 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton1 = New System.Windows.Forms.RadioButton()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 43)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(87, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Digite un numero"
+        Me.Label1.Visible = False
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(105, 40)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox1.TabIndex = 1
+        Me.TextBox1.Visible = False
+        '
+        'ListBox1
+        '
+        Me.ListBox1.BackColor = System.Drawing.Color.White
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Location = New System.Drawing.Point(346, 73)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(201, 238)
+        Me.ListBox1.TabIndex = 5
+        Me.ListBox1.Visible = False
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(225, 40)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(84, 23)
+        Me.Button1.TabIndex = 3
+        Me.Button1.Text = "Mostrar tabla"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(403, 317)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 4
+        Me.Button2.Text = "Limpiar"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(89, 40)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(130, 21)
+        Me.ComboBox1.TabIndex = 7
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(225, 40)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(84, 23)
+        Me.Button3.TabIndex = 8
+        Me.Button3.Text = "Mostrar tabla"
+        Me.Button3.UseVisualStyleBackColor = True
+        Me.Button3.Visible = False
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.BackColor = System.Drawing.Color.White
+        Me.GroupBox1.Controls.Add(Me.RadioButton3)
+        Me.GroupBox1.Controls.Add(Me.Button4)
+        Me.GroupBox1.Controls.Add(Me.RadioButton2)
+        Me.GroupBox1.Controls.Add(Me.RadioButton1)
+        Me.GroupBox1.Location = New System.Drawing.Point(55, 134)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(208, 158)
+        Me.GroupBox1.TabIndex = 9
+        Me.GroupBox1.TabStop = False
+        '
+        'RadioButton3
+        '
+        Me.RadioButton3.AutoSize = True
+        Me.RadioButton3.Location = New System.Drawing.Point(25, 99)
+        Me.RadioButton3.Name = "RadioButton3"
+        Me.RadioButton3.Size = New System.Drawing.Size(84, 17)
+        Me.RadioButton3.TabIndex = 2
+        Me.RadioButton3.TabStop = True
+        Me.RadioButton3.Text = "Tabla del 13"
+        Me.RadioButton3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(127, 129)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(75, 23)
+        Me.Button4.TabIndex = 10
+        Me.Button4.Text = "Calcular"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'RadioButton2
+        '
+        Me.RadioButton2.AutoSize = True
+        Me.RadioButton2.Location = New System.Drawing.Point(25, 66)
+        Me.RadioButton2.Name = "RadioButton2"
+        Me.RadioButton2.Size = New System.Drawing.Size(84, 17)
+        Me.RadioButton2.TabIndex = 1
+        Me.RadioButton2.TabStop = True
+        Me.RadioButton2.Text = "Tabla del 12"
+        Me.RadioButton2.UseVisualStyleBackColor = True
+        '
+        'RadioButton1
+        '
+        Me.RadioButton1.AutoSize = True
+        Me.RadioButton1.Location = New System.Drawing.Point(25, 33)
+        Me.RadioButton1.Name = "RadioButton1"
+        Me.RadioButton1.Size = New System.Drawing.Size(84, 17)
+        Me.RadioButton1.TabIndex = 0
+        Me.RadioButton1.TabStop = True
+        Me.RadioButton1.Text = "Tabla del 11"
+        Me.RadioButton1.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(15, 317)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(115, 23)
+        Me.Button5.TabIndex = 11
+        Me.Button5.Text = "Cerrar programa"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Silver
+        Me.BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), System.Drawing.Image)
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.ClientSize = New System.Drawing.Size(337, 373)
+        Me.ControlBox = False
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Form1"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents ListBox1 As System.Windows.Forms.ListBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents ComboBox1 As System.Windows.Forms.ComboBox
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents RadioButton3 As System.Windows.Forms.RadioButton
+    Friend WithEvents RadioButton2 As System.Windows.Forms.RadioButton
+    Friend WithEvents RadioButton1 As System.Windows.Forms.RadioButton
+    Friend WithEvents Button4 As System.Windows.Forms.Button
+    Friend WithEvents Button5 As System.Windows.Forms.Button
+
+End Class
---
+++ WindowsApplication3/Form1.vb
@@ -0,0 +1,204 @@
+﻿Public Class Form1
+    Dim num, x As Integer
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        'ListBox1.ClearSelected()
+        'num = Val(TextBox1.Text)
+        If ComboBox1.SelectedIndex = 1 Then
+            For a = 1 To 10
+                ListBox1.Items.Add(1 & "*" & a & "=" & 1 * a)
+                ListBox1.Visible = True
+                Size = New Size(599, 412)
+
+            Next
+        End If
+        If ComboBox1.SelectedIndex = 2 Then
+            For a = 1 To 10
+                ListBox1.Items.Add(2 & "*" & a & "=" & 2 * a)
+                ListBox1.Visible = True
+                Size = New Size(599, 412)
+
+            Next
+
+        End If
+        If ComboBox1.SelectedIndex = 3 Then
+            For a = 1 To 10
+                ListBox1.Items.Add(3 & "*" & a & "=" & 3 * a)
+                ListBox1.Visible = True
+                Size = New Size(599, 412)
+
+            Next
+
+        End If
+        If ComboBox1.SelectedIndex = 4 Then
+            For a = 1 To 10
+                ListBox1.Items.Add(4 & "*" & a & "=" & 4 * a)
+                ListBox1.Visible = True
+                Size = New Size(599, 412)
+
+            Next
+
+        End If
+        If ComboBox1.SelectedIndex = 5 Then
+            For a = 1 To 10
+                ListBox1.Items.Add(5 & "*" & a & "=" & 5 * a)
+                ListBox1.Visible = True
+                Size = New Size(599, 412)
+
+            Next
+
+        End If
+        If ComboBox1.SelectedIndex = 6 Then
+            For a = 1 To 10
+                ListBox1.Items.Add(6 & "*" & a & "=" & 6 * a)
+                ListBox1.Visible = True
+                Size = New Size(599, 412)
+
+            Next
+
+        End If
+        If ComboBox1.SelectedIndex = 7 Then
+            For a = 1 To 10
+                ListBox1.Items.Add(7 & "*" & a & "=" & 7 * a)
+                ListBox1.Visible = True
+                Size = New Size(599, 412)
+
+            Next
+        End If
+        If ComboBox1.SelectedIndex = 8 Then
+            For a = 1 To 10
+                ListBox1.Items.Add(8 & "*" & a & "=" & 8 * a)
+                ListBox1.Visible = True
+                Size = New Size(599, 412)
+
+            Next
+
+        End If
+        If ComboBox1.SelectedIndex = 9 Then
+            For a = 1 To 10
+                ListBox1.Items.Add(9 & "*" & a & "=" & 9 * a)
+                ListBox1.Visible = True
+                Size = New Size(599, 412)
+
+            Next
+
+        End If
+        If ComboBox1.SelectedIndex = 10 Then
+            For a = 1 To 10
+                ListBox1.Items.Add(10 & "*" & a & "=" & 10 * a)
+                ListBox1.Visible = True
+                Size = New Size(599, 412)
+
+            Next
+
+        End If
+
+        If ComboBox1.SelectedIndex = 11 Then
+            MsgBox("Por favor escriba el numero en el cuadro de texto", MsgBoxStyle.Critical, "Nuevo número")
+
+            Label1.Visible = True
+            TextBox1.Visible = True
+            TextBox1.Focus()
+            Button3.Visible = True
+            Button1.Visible = False
+
+
+        End If
+        ComboBox1.Visible = False
+        Button1.Visible = False
+
+        '  ListBox1.ClearSelected()
+
+        ' Size = New Size(599, 412)
+
+    End Sub
+
+
+   
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        ComboBox1.Items.Add("Seleccione un numero")
+        ComboBox1.Items.Add("1")
+        ComboBox1.Items.Add("2")
+        ComboBox1.Items.Add("3")
+        ComboBox1.Items.Add("4")
+        ComboBox1.Items.Add("5")
+        ComboBox1.Items.Add("6")
+        ComboBox1.Items.Add("7")
+        ComboBox1.Items.Add("8")
+        ComboBox1.Items.Add("9")
+        ComboBox1.Items.Add("10")
+        ComboBox1.Items.Add("Otro número")
+        ComboBox1.SelectedIndex = 0
+
+    End Sub
+
+
+    Private Sub Button3_Click_1(sender As Object, e As EventArgs) Handles Button3.Click
+        ListBox1.Visible = True
+        ListBox1.ClearSelected()
+        num = Val(TextBox1.Text)
+        For a = 1 To 10
+            ListBox1.Items.Add(num & "*" & a & "=" & TextBox1.Text * a)
+
+        Next
+        Label1.Visible = False
+        TextBox1.Visible = False
+        Button3.Visible = False
+        Size = New Size(599, 412)
+
+
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        ListBox1.Items.Clear()
+        ComboBox1.Visible = True
+        Button1.Visible = True
+        ComboBox1.SelectedIndex = 0
+        Size = New Size(339, 412)
+        RadioButton1.Checked = False
+        RadioButton2.Checked = False
+        RadioButton3.Checked = False
+        TextBox1.Clear()
+
+
+
+
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        If RadioButton1.Checked Then
+            For a = 1 To 10
+                ListBox1.Items.Add(11 & "*" & a & "=" & 11 * a)
+                ListBox1.Visible = True
+                Size = New Size(599, 412)
+
+            Next
+        End If
+        If RadioButton2.Checked Then
+            For a = 1 To 10
+                ListBox1.Items.Add(12 & "*" & a & "=" & 12 * a)
+                ListBox1.Visible = True
+                Size = New Size(599, 412)
+            Next
+        End If
+        If RadioButton3.Checked Then
+            For a = 1 To 10
+                ListBox1.Items.Add(13 & "*" & a & "=" & 13 * a)
+                ListBox1.Visible = True
+                Size = New Size(599, 412)
+            Next
+        End If
+    End Sub
+
+   
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+
+        Me.Close()
+
+    End Sub
+
+    
+    
+End Class
